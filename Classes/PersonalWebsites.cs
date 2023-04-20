5e0e2f9eca5b907d4bd4c495ea1922ea05b7d39f
Wahid Shuvo <wshuvo360@gmail.com>
2023-04-20 21:34:46 +0600

    update 20.4

--- Classes/PersonalWebsites.cs
+++ Classes/PersonalWebsites.cs
@@ -12,5 +12,34 @@ namespace media.Classes
         private string link;
         private string name;
         private Image logo;
+        public string Link
+        {
+            get { return this.link; }
+            set { this.link = value; }
+        }
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if ((Link.ToLower()).Contains("facebook")) this.name = "Facebook";
+                else if (Link.Contains("whatsapp")) this.name = "Whatsapp";
+                else if (Link.Contains("linkedin")) this.name = "LinkedIn";
+                else if (Link.Contains("instagram")) this.name = "Instagram";
+                else if (Link.Contains("twiter")) this.name = "Twiter";
+                else this.name = "Unidentified";
+            }
+        }
+        public Image Logo
+        {
+            get { return this.logo; }
+            set { 
+                if (this.Name == "Facebook") this.logo = global::media.Properties.Resources.facebook; 
+                if (this.Name == "LinkedIn") this.logo = global::media.Properties.Resources.linkedin;
+                if (this.Name == "Instagram") this.logo = global::media.Properties.Resources.instagram;
+                else this.logo = global::media.Properties.Resources.logo;
+            }
+        }
+
     }
 }
--- Classes/User.cs
+++ Classes/User.cs
@@ -16,9 +16,79 @@ namespace media.Classes
         private string password;
         private string phoneNumber;
         private DateTime dob;
-        private char gender;
+        private string gender;
         private Image profilePhoto;
         private PersonalWebsites[] websites;
         private User[] friends;
+        public string UserFirstName
+        {
+            get { return this.userFirstName; }
+            set { this.userFirstName = value; }
+        }
+        public string UserLastName
+        {
+            get { return this.userLastName; }
+            set { this.userLastName = value; }
+        }
+        public string Bio
+        {
+            get { return this.bio; }
+            set
+            {
+                this.bio = value.Substring(0,100);
+            }
+        }
+        public string Email
+        {
+            get { return this.email; }
+            set { this.email = value; }
+        }
+        public string Password
+        {
+            get { return this.password; }
+            set { this.password = value; }
+        }
+        public string PhoneNumber
+        {
+            get { return this.phoneNumber; }
+            set
+            {
+                this.phoneNumber = value;
+            }
+        }
+        public DateTime Dob
+        {
+            get { return this.dob; }
+            set
+            {
+                this.dob = value;
+            }
+        }
+        public string Gender
+        {
+            get { return this.gender; }
+            set
+            {
+                this.gender = value;
+            }
+        }
+        public Image ProfilePhoto
+        {
+            get { return this.profilePhoto; }
+            set
+            {
+                this.profilePhoto = value;
+            }
+        }
+        
+        public User(string userFirstName, string userLastName, string email, string phoneNumber, DateTime dob, string gender) 
+        { 
+            this.UserFirstName= userFirstName;
+            this.UserLastName= userLastName;
+            this.Email= email;
+            this.PhoneNumber= phoneNumber;
+            this.Dob= dob;
+            this.Gender=gender;
+        }
     }
 }
